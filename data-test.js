9f939d8d13d599d47573f91d4b6af582bac6273f
kaa-serpent <guillaumedecadoudal@gmail.com>
2025-11-23 03:09:31 +0100

    CSV TO JS

---
+++ data-test.js
@@ -0,0 +1,28 @@
+const data = [
+  {
+    "marque_vehicule": "Acura",
+    "modele_vehicule": "CL (Gen 1 / YA1) 1996 à 1999",
+    "lishi": "fake",
+    "t3": "fake",
+    "ic_card": "fake",
+    "silca_card": "fake",
+    "silca_key_blank": "fake",
+    "ilca_key_blank": "fake",
+    "ilco_key_blank": "fake",
+    "keyline_key_blank": "fake",
+    "jma_key_blank": "fake"
+  },
+  {
+    "marque_vehicule": "Acura",
+    "modele_vehicule": "CL (Gen 2 / YA4) 2000 à 2003",
+    "lishi": "fake",
+    "t3": "fake",
+    "ic_card": "fake",
+    "silca_card": "fake",
+    "silca_key_blank": "fake",
+    "ilca_key_blank": "fake",
+    "ilco_key_blank": "fake",
+    "keyline_key_blank": "fake",
+    "jma_key_blank": "fake"
+  }
+]
